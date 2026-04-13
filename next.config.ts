4311eb5b8b71dfc4ecef8d05cb423c5007e8ef50
ReactDev81 <reactdev81@gmail.com>
2026-04-13 13:35:53 +0530

    chore: update next-pwa dependency to @ducanh2912/next-pwa@10.2.9

--- next.config.ts
+++ next.config.ts
@@ -1,29 +1,59 @@
-const withPWA = require('next-pwa');
+// const withPWA = require('next-pwa');
 
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+//   output: "standalone",
+//   compiler: {
+//     removeConsole: process.env.NODE_ENV !== "development"
+//   },
+//   turbopack: {},
+//   images: {
+//     remotePatterns: [
+//       {
+//         protocol: 'https',
+//         hostname: 'telehealthwebapplive.cmcludhiana.in',
+//         port: '',
+//         pathname: '/storage/**',
+//       },
+//     ],
+//   },
+//   pwa: {
+//     dest: "public",
+//     register: true,
+//     skipWaiting: true,
+//     disable: process.env.NODE_ENV === 'development',
+//   },
+// };
+
+// export default process.env.NODE_ENV === 'development' ? nextConfig : withPWA(nextConfig);
+
+
+import withPWAInit from "@ducanh2912/next-pwa";
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
   reactStrictMode: true,
   output: "standalone",
   compiler: {
-    removeConsole: process.env.NODE_ENV !== "development"
+    removeConsole: process.env.NODE_ENV !== "development",
   },
-  turbopack: {},
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'telehealthwebapplive.cmcludhiana.in',
-        port: '',
-        pathname: '/storage/**',
+        protocol: "https",
+        hostname: "telehealthwebapplive.cmcludhiana.in",
+        pathname: "/**",
       },
     ],
   },
-  pwa: {
-    dest: "public",
-    register: true,
-    skipWaiting: true,
-    disable: process.env.NODE_ENV === 'development',
-  },
+  turbopack: {},
 };
 
-export default process.env.NODE_ENV === 'development' ? nextConfig : withPWA(nextConfig);
+const withPWA = withPWAInit({
+  dest: "public",
+  register: true,
+  disable: process.env.NODE_ENV === "development",
+});
+
+export default withPWA(nextConfig);
